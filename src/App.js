71c6f55e8065923717cba361f627a6d6511eadd6
tharmigatechneapp <tharmiga@techneapp.com>
2023-01-29 01:26:21 +0530

    change folder structure

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import Header from './Header/Header';
-import Home from './Screens/Home';
-import Places from "./Screens/Places";
-import Hotels from "./Screens/Hotels";
-import About_us from "./Screens/About_us";
-import Login from './Screens/Login';
-import Footer from './Footer/Footer';
-import "..//src/Images/tourism_news.jpg";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Places from "./components/Places/Places";
+import Hotels from "./components/Hotels/Hotels";
+import About_us from "./components/Aboutus/About_us";
+import Login from "./components/Login/Login";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  let component
+  let component;
   switch (window.location.pathname) {
     case "/Home":
-      component = <Home/>
+      component = <Home />;
+
       break;
     case "/Places":
-      component = <Places/>
+      component = <Places />;
       break;
     case "/Hotels":
-      component = <Hotels/>
+      component = <Hotels />;
       break;
     case "/About_us":
-      component = <About_us/>
+      component = <About_us />;
       break;
     case "/Login":
-      component = <Login/>
+      component = <Login />;
       break;
-  
+
     default:
-      component = <Home/>
+      component = <Home />;
       break;
   }
   return (
     <>
-    <Header/>
-    {component}
-    <Footer />
-
+      <Header />
+      {component}
+      <Footer />
     </>
   );
 }
